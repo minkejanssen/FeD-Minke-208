24eeae9b2d8592b509d88ac3ba4fe2f004f022c0
minkejanssen <minke.janssen@hva.nl>
2025-09-17 22:12:48 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 console.log("hi");
+
+const zichtbaarheid = document.querySelectorAll('.vervagen');
+
+// https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp en chatgpt // 
+const togglebutton = document.getElementById('togglebutton');
+
+
+togglebutton.addEventListener('click', () => {
+    document.classList.toggle ("darkmode")
+})

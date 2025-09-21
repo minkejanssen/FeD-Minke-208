2dafe89dde58f62929073be706524cfaef31ae20
minkejanssen <minke.janssen@hva.nl>
2025-09-21 10:18:34 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -4,13 +4,15 @@ console.log("hi");
 
 
 
-
+let Lettergrootte = document.querySelector("h3")
 
 let Welkom = document.getElementById("welkom");
 
+document.querySelector("h3").style.fontSize = "20px";
+
 // voorbeeld code van mijn opdracht van inleiding programmeren van jaar 1 //
 function veranderTekst() {
-    Welkom.textContent = "Welkom bij Joe & the Juice, scroll verder voor meer informatie!"
+    Welkom.textContent = "Welcome to Joe & the Juice, scroll down to learn more!"
 }
 
 Welkom.addEventListener("click", veranderTekst);
